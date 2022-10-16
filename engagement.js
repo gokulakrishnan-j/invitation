8d04ad04dc287e10644ee05468665a83921e21d3
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2022-10-16 15:06:56 +0530

    Update engagement.js

--- engagement.js
+++ engagement.js
@@ -1,6 +1,6 @@
 var names=document.getElementById('names')
 names.addEventListener('click',()=>{
     names.style.backgroundColor="rgba(151, 45, 45, 0.959)"
-    names.innerHTML=`<span class="jsName">VASU DEVAN & PRIYA</span>`
+    names.innerHTML=`<span class="jsName">VASUDEVAN & PRIYA</span>`
     document.getElementById('edit_role').style.display="none"
 })
